7222126a6fb9c9cf79fd80d7d7e5bfb5526e527f
Mark Channer <markchanner@gmail.com>
2018-05-02 14:11:24 +0100

    Displays names 'Tekken Style'

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'testing infrastructure working'
+    erb(:index)
+  end
+
+  post '/names' do
+    @p1_name = params[:p1_name]
+    @p2_name = params[:p2_name]
+    erb(:play)
   end
 
   # start the server if ruby file executed directly
--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -4,7 +4,7 @@ feature 'players can fill in names and see on-screen' do
     fill_in 'p1_name', with: 'Mark'
     fill_in 'p2_name', with: 'The Beast'
     click_on 'submit'
-    expect(page).to have_content('Mark')
-    expect(page).to have_content('The Beast')
+    expect(page).to have_content('Mark vs The Beast')
+    # expect(page).to have_content('The Beast')
   end
 end
