aa4ef7d5a2122e00a78ea162e55c1b2a96210c3b
Mark Channer <markchanner@gmail.com>
2018-05-02 14:42:36 +0100

    Uses session to store player names across requests

--- app.rb
+++ app.rb
@@ -1,13 +1,22 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @p1_name = params[:p1_name]
-    @p2_name = params[:p2_name]
+    session[:p1_name] = params[:p1_name]
+    session[:p2_name] = params[:p2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @p1_name = session[:p1_name]
+    @p2_name = session[:p2_name]
     erb(:play)
   end
 
