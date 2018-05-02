cff3cde74537a8d079954abf3bd562b347f0d505
Mark Channer <markchanner@gmail.com>
2018-05-02 14:09:24 +0100

    Takes player names in form

--- spec/features/homepage_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'homepage working' do
-  scenario 'displays testing infrastructure working' do
-    visit('/')
-    expect(page).to have_content('testing infrastructure working')
-  end
-end
---
+++ spec/features/player_names_spec.rb
@@ -0,0 +1,10 @@
+feature 'players can fill in names and see on-screen' do
+  scenario 'fill in names' do
+    visit('/')
+    fill_in 'p1_name', with: 'Mark'
+    fill_in 'p2_name', with: 'The Beast'
+    click_on 'submit'
+    expect(page).to have_content('Mark')
+    expect(page).to have_content('The Beast')
+  end
+end
