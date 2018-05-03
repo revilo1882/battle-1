8fddc715733d0ed966b715a8c648951e7a0ff845
Revilo1882 <oliverrharris@gmail.com>
2018-05-03 12:23:28 +0100

    adds test for coverage of attack method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,20 +1,27 @@
 require 'player'
 
 describe Player do
-  subject(:player) { described_class.new('The Beast') }
+  subject(:the_beast) { described_class.new('The Beast') }
+  subject(:mark) { described_class.new('Mark') }
   describe '#name' do
-    it 'returns the players name' do
-      expect(player.name).to eq('The Beast')
+    it "returns the player's name" do
+      expect(the_beast.name).to eq('The Beast')
     end
   end
 
   describe '#health' do
     it "displays a player's health" do
-      expect(player.health).to eq(described_class::MAX_HEALTH)
+      expect(the_beast.health).to eq(described_class::MAX_HEALTH)
     end
 
     it "deducts a player's health" do
-      expect { player.reduce_health }.to change{ player.health }.by(-described_class::HEALTH_DEDUCTION)
+      expect { the_beast.reduce_health }.to change{ the_beast.health }.by(-described_class::HEALTH_DEDUCTION)
+    end
+  end
+
+  describe '#attack' do
+    it "deducts a player's health when attacked" do
+      expect { mark.attack(the_beast) }.to change{ the_beast.health }.by(-described_class::HEALTH_DEDUCTION)
     end
   end
 end
